017f08415f2ab5430e75f3d45d180fef34f25233
John Idi <marghispecial@gmail.com>
2022-09-12 11:21:18 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+*main- Serves as entry point for the program
+*
+Return: By default returns zero if no erros otherwise returns non-zero value.
+*/
+int main(void)
+{
+ put("\"Programming is like building a multilingual puzzle");
+ return (0);
+}
