dff632424bb099286694c73ac6ae677b3c538c3e
John Idi <marghispecial@gmail.com>
2022-09-12 11:59:53 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ Return: By default returns zero if no erros otherwise returns non-zero value.
 */
 int main(void)
 {
- put("\"Programming is like building a multilingual puzzle");
- return (0);
+put("\"Programming is like building a multilingual puzzle");
+return (0);
 }
