32e3bf8f6caafd4698aca9366e34224aab4dd441
John Idi <marghispecial@gmail.com>
2022-09-21 08:15:19 -0700

    tasK

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
-* main - assign a random number to the variable n each time it is executed
-* if the number is greater than 0: is positive, if the number is 0: is zero
-* if the number is less than 0: is negative, follow by new line
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+/* your code goes there */
 if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else if (n < 0)
+if (n < 0)
 {
 printf("%d is negative\n", n);
 }
-else
+if (n == 0)
 {
 printf("%d is zero\n", n);
 }
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-*reset_to_98 - resets the given value to integer 98
-*@n: paremeters
-*Return : nothing
-/*
+ * reset_to_98 - main
+ * @n: - pinter to int
+ */
 
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
+
