687d2cca63e2886027f21164685f6506f0bae0b9
John Idi <marghispecial@gmail.com>
2022-09-28 03:04:38 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /*base case */
+	if (*s == '\0') /* base case */
 	{
 		_putchar('\n');
 		return;
 	}
-		_putchar(*s);
-		_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
