89fba51efb6f85a2fda868f12861a12a95af73e3
John Idi <marghispecial@gmail.com>
2022-10-06 14:48:10 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,4 @@
-#include "main_h"
+#include "main.h"
 
 /**
  *_calloc - allocated memory for nmemb elements of size bytes
@@ -10,14 +10,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *p;
 unsigned int i;
-
 if (nmemb == 0 || size == 0)
 return (NULL);
-
 p = malloc(nmemb * size);
 if (p == NULL);
 return (NULL);
-
 for (i = 0; i < nmemb * size; i++)
 p[i] = 0;
 return (p);
