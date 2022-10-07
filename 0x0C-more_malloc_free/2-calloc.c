a4488d1515c1e9dc189ba247ab919522dc1ef561
John Idi <marghispecial@gmail.com>
2022-10-07 00:54:22 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,7 +6,6 @@
  *@size: bytes for each position in array 
  *Return: pointer void
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *p;
@@ -16,7 +15,7 @@ if (nmemb == 0 || size == 0)
 return (NULL);
 
 p = malloc(nmemb * size);
-if (p == NULL);
+if (p == NULL)
 return (NULL);
 
 for (i = 0; i < nmemb * size; i++)
