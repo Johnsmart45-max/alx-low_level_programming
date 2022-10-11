75f06ecf4c68f5a3fdae14f9b648f25736952bb5
John Idi <marghispecial@gmail.com>
2022-10-10 20:12:51 -0700

    Task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,11 @@
-#include "dog.h"                                                    
+#include "dog.h"
 #include <stdlib.h>
 
-/**                                                 
+/**                                 
  * free_dog - Frees dogs.
  * @d: The dog to be freed.
  */
-void free_dog(dog_t *d) 
+void free_dog(dog_t *d)
 {
        	if (d == NULL)
 		return;
