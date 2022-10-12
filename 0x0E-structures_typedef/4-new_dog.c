a3e5bf613a2a1b088cca516c20535451d5919ae3
John Idi <marghispecial@gmail.com>
2022-10-11 20:23:07 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,43 @@
 #include "dog.h"
 #include <stdlib.h>
 
+int _strlen(char *str);
+char *_strcopy(char *dest, char *spc);
+dog_t *new_dog(char *name, float age, char *owner);
+
 /**
- * free_dog - Frees dogs.
- * @d: The dog to be freed.
+ * _strlen - finds the lenth of a string.
+ * @str: The strings to be measured.
+ *
+ * Return: The length of the string.
  */
-void free_dog(dog_t *d)
+int _strlen(char *str)
 {
-	if (d == NULL)
-		return;
-	free(d->owner);
-	free(d->name);
-	free(d);
+ 	int len = 0;
+
+	while (str++)
+		len++;
+
+	return (len);
 }
+
+/* * _strcopy - copies a string pointed to by src, including null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source is string
+ *
+ * Return; The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int (index = 0; src[index]; index++)
+		dest[index] = src[index] = src[index];
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of dog.
+ *
+ * Return: the new strunct dog.
